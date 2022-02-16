9d88c2567b73340d0110d34f0b2adb5c616de1ad
ella-t <58817053+ella-t@users.noreply.github.com>
2022-02-16 14:35:06 +0000

    Added touch input

--- Assets/Scripts/3DView/FirstPersonController.cs
+++ Assets/Scripts/3DView/FirstPersonController.cs
@@ -38,6 +38,17 @@ public class FirstPersonController : MonoBehaviour
     private Interactable Selection = null;
     private bool TryingToExitCameraLock = false;
 
+    private struct Click
+    {
+        public enum ClickPhase
+        {
+            Begin,
+            End
+        }
+        public Vector3 position;
+        public ClickPhase phase;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -85,39 +96,44 @@ public class FirstPersonController : MonoBehaviour
                 MainCamera.transform.Rotate(0, SwivelSpeed * Time.deltaTime, 0);
             }
 
-            // Begin a click
-            if (Input.GetMouseButtonDown(0))
+            // Get all beginning or ending mouse clicks or touch "clicks"
+            List<Click> clicks = GetClicks();
+
+            for (int i = 0; i < clicks.Count; i++)
             {
-
-                RaycastHit clicked;
-                Ray mouseRay = MainCamera.ScreenPointToRay(Input.mousePosition);
-
-                if (Physics.Raycast(mouseRay, out clicked))
+                // Click is beginning
+                if (clicks[i].phase == Click.ClickPhase.Begin)
                 {
-                    if (clicked.transform.gameObject.GetComponent<Interactable>())
+                    RaycastHit clicked;
+                    Ray mouseRay = MainCamera.ScreenPointToRay(clicks[i].position);
+
+                    if (Physics.Raycast(mouseRay, out clicked))
                     {
-                        // Player started clicking on an interactable
-                        Selection = clicked.transform.gameObject.GetComponent<Interactable>();
+                        if (clicked.transform.gameObject.GetComponent<Interactable>())
+                        {
+                            // Player started clicking on an interactable
+                            Selection = clicked.transform.gameObject.GetComponent<Interactable>();
+                        }
                     }
                 }
-            }
-            // End a click
-            if (Input.GetMouseButtonUp(0))
-            {
-                RaycastHit clicked;
-                Ray mouseRay = MainCamera.ScreenPointToRay(Input.mousePosition);
-
-                if (Physics.Raycast(mouseRay, out clicked))
+                // Click is ending
+                if (clicks[i].phase == Click.ClickPhase.End)
                 {
-                    if (Selection != null && clicked.transform.gameObject.GetComponent<Interactable>() == Selection)
-                    {
-                        // Player clicked fully on a specific interactable
-                        Selection.OnClick();
-                        Selection = null;
-                    }
-                }
+                    RaycastHit clicked;
+                    Ray mouseRay = MainCamera.ScreenPointToRay(clicks[i].position);
 
-                Selection = null;
+                    if (Physics.Raycast(mouseRay, out clicked))
+                    {
+                        if (Selection != null && clicked.transform.gameObject.GetComponent<Interactable>() == Selection)
+                        {
+                            // Player clicked fully on a specific interactable
+                            Selection.OnClick();
+                            Selection = null;
+                        }
+                    }
+
+                    Selection = null;
+                }
             }
 
         }
@@ -127,37 +143,77 @@ public class FirstPersonController : MonoBehaviour
             ExitCameraLockMarginPixelsX = Mathf.RoundToInt(Screen.width / 100.0f * ExitCameraLockMargin);
             ExitCameraLockMarginPixelsY = Mathf.RoundToInt(Screen.height / 100.0f * ExitCameraLockMargin);
 
-            if (Input.GetMouseButtonDown(0))
-            {
-                
-                // Check if player is trying to exit camera lock
-                if (Input.mousePosition.x < ExitCameraLockMarginPixelsX
-                    || Input.mousePosition.x > Screen.width - ExitCameraLockMarginPixelsX
-                    || Input.mousePosition.y < ExitCameraLockMarginPixelsY
-                    || Input.mousePosition.y > Screen.height - ExitCameraLockMarginPixelsY)
-                {
-                    TryingToExitCameraLock = true;
-                }
+            List<Click> clicks = GetClicks();
 
-            }
-
-            if (Input.GetMouseButtonUp(0))
+            for (int i = 0; i < clicks.Count; i++)
             {
-                if (Input.mousePosition.x < ExitCameraLockMarginPixelsX
-                    || Input.mousePosition.x > Screen.width - ExitCameraLockMarginPixelsX
-                    || Input.mousePosition.y < ExitCameraLockMarginPixelsY
-                    || Input.mousePosition.y > Screen.height - ExitCameraLockMarginPixelsY)
+                if (clicks[i].phase == Click.ClickPhase.Begin)
                 {
-                    if (TryingToExitCameraLock)
+
+                    // Check if player is trying to exit camera lock
+                    if (clicks[i].position.x < ExitCameraLockMarginPixelsX
+                        || clicks[i].position.x > Screen.width - ExitCameraLockMarginPixelsX
+                        || clicks[i].position.y < ExitCameraLockMarginPixelsY
+                        || clicks[i].position.y > Screen.height - ExitCameraLockMarginPixelsY)
                     {
-                        ReturnCameraToOriginalPositionRotation();
+                        TryingToExitCameraLock = true;
                     }
+
                 }
-                TryingToExitCameraLock = false;
+
+                if (clicks[i].phase == Click.ClickPhase.End)
+                {
+                    if (clicks[i].position.x < ExitCameraLockMarginPixelsX
+                        || clicks[i].position.x > Screen.width - ExitCameraLockMarginPixelsX
+                        || clicks[i].position.y < ExitCameraLockMarginPixelsY
+                        || clicks[i].position.y > Screen.height - ExitCameraLockMarginPixelsY)
+                    {
+                        if (TryingToExitCameraLock)
+                        {
+                            ReturnCameraToOriginalPositionRotation();
+                        }
+                    }
+                    TryingToExitCameraLock = false;
+                }
             }
 
         }
 
+    }
+
+    private List<Click> GetClicks()
+    {
+        List<Click> clicks = new List<Click>();
+
+        // Get touch clicks
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Click touchClick = new Click();
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+                touchClick.phase = Click.ClickPhase.Begin;
+            else if (Input.GetTouch(i).phase == TouchPhase.Ended)
+                touchClick.phase = Click.ClickPhase.End;
+            touchClick.position = Input.GetTouch(i).position;
+            clicks.Add(touchClick);
+        }
+
+        // Get mouse clicks
+        if (Input.GetMouseButtonDown(0))
+        {
+            Click mouseClick = new Click();
+            mouseClick.phase = Click.ClickPhase.Begin;
+            mouseClick.position = Input.mousePosition;
+            clicks.Add(mouseClick);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Click mouseClick = new Click();
+            mouseClick.phase = Click.ClickPhase.End;
+            mouseClick.position = Input.mousePosition;
+            clicks.Add(mouseClick);
+        }
+
+        return clicks;
     }
 
     public void PointCameraAt(Transform target, float offset)
