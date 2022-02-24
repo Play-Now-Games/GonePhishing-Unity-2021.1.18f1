94eea07ed2a1b5d2112d2b3175c79747bc564d98
Avery <56042414+Othelbark@users.noreply.github.com>
2022-02-24 00:01:06 +0000

    Built out NotesApp class
    Added notes as a list of strings and AddNote/RemoveNote functions

--- Assets/Scripts/Apps/NotesApp.cs
+++ Assets/Scripts/Apps/NotesApp.cs
@@ -1,19 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NotesApp : App
-{ 
+{
+    public List<string> notes;
+
+    [SerializeField]
+    private Text notesDisplay;
     
     // Start is called before the first frame update
     new void Start()
     {
         base.Start();
+
+
+        UpdateDisplay();
     }
 
     // Update is called once per frame
     new void Update()
     {
         base.Update();
+    }
+
+    public void UpdateDisplay()
+    {
+        string newDisplay = "";
+
+        foreach (string note in notes)
+        {
+            newDisplay += note + "\n";
+        }
+
+        notesDisplay.text = newDisplay;
+    }
+
+    public void AddNote(string note)
+    {
+        notes.Add(note);
+
+        UpdateDisplay();
+    }
+
+    public void RemoveNote(string note)
+    {
+        if (notes.Contains(note))
+        {
+            notes.Remove(note);
+        }
+
+        UpdateDisplay();
     }
 }
