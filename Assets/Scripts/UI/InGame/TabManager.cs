7ef041bc68d63dddb22a5fa97d91b511050ec577
Avery <56042414+Othelbark@users.noreply.github.com>
2022-04-25 22:26:01 +0100

    Tab switching now with "Sound"!

--- Assets/Scripts/UI/InGame/TabManager.cs
+++ Assets/Scripts/UI/InGame/TabManager.cs
@@ -7,11 +7,23 @@ public class TabManager : MonoBehaviour
 
     public GameObject[] tabs;
 
+
+    private SoundsHolder _audioScript;
+
+    public void Start()
+    {
+        //GetAudio Source
+        GameObject speakers = GameObject.FindGameObjectWithTag("Speakers");
+        _audioScript = speakers.GetComponent<SoundsHolder>();
+    }
+
     public void OpenTab(int index)
     {
         if (tabs.Length > index)
         {
             tabs[index].transform.SetSiblingIndex(tabs.Length);
+
+            _audioScript.PlayClick();
         }
         else
         {
