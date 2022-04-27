db4d1d41b1e6d278ecd01405681a3afc05b015d2
Avery <56042414+Othelbark@users.noreply.github.com>
2022-04-27 19:33:04 +0100

    Revert "PopUpBack"
    This reverts commit 73ed60ffe40bbd8ae38e34a8071149573c16856f.

--- Assets/Scripts/Player/Main.cs
+++ Assets/Scripts/Player/Main.cs
@@ -493,7 +493,7 @@ public class Main : MonoBehaviour
 
         #region Search For Repetitive PopUps On Scene
 
-        if (totalEmails.Length != 1)
+        if ((Pops.Length < (int)popUpLimiter) && (totalEmails.Length != 1))
         {
             for (int i = 0; i < Pops.Length; i++)
             {
